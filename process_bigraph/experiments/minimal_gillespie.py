616f5d45842284f0443d97161cddd43e75ed89f1
Eran <agmon.eran@gmail.com>
2023-08-01 18:17:42 -0400

    remove sed demo, rename toys to minimal_gillespie

--- process_bigraph/experiments/minimal_gillespie.py
+++ process_bigraph/experiments/minimal_gillespie.py
@@ -123,67 +123,6 @@ class GillespieEvent(Process):
         return update
 
 
-class EstimateParameters(Step):
-    defaults = {}
-
-    def __init__(self, config=None):
-        super().__init__(config)
-
-    def schema(self):
-        return {
-            'inputs': {
-                'data': 'tree[any]',
-                'model': 'sbml'},
-            'outputs': {
-                'parameters': 'tree[any]'
-            }
-        }
-
-    def update(self, input):
-        return {}
-
-
-class UniformTimecourse(Step):
-    defaults = {}
-
-    def __init__(self, config=None):
-        super().__init__(config)
-
-    def schema(self):
-        return {
-            'inputs': {
-                'model': 'sbml',
-                'parameters': 'tree[any]'
-            },
-            'outputs': {
-                'simulation_results': 'tree[any]'
-            }
-        }
-
-    def update(self, input):
-        return {}
-
-
-class AnalyzeResults(Step):
-    defaults = {}
-
-    def __init__(self, config=None):
-        super().__init__(config)
-
-    def schema(self):
-        return {
-            'inputs': {
-                'simulation_results': 'tree[any]'
-            },
-            'outputs': {
-                'analysis_results': 'tree[any]'
-            }
-        }
-
-    def update(self, input):
-        return {}
-
-
 def test_gillespie_composite():
     GillespieComposite = Generator({
         'composition': {
@@ -237,77 +176,9 @@ def test_gillespie_composite():
     gillespie.update({'DNA': {'G': 11.0}, 'mRNA': {'C': 5.0}}, 10000.0)
 
 
-def test_sed_composite():
-
-    # TODO - perhaps we should not register directly to the base "type_registry" systems but rather pass it in to the composite?
-    types.type_registry.register('sbml', {
-        '_super': 'string',
-        '_default': '"something.sbml"',
-        '_description': 'sbml model file'})
-
-    workflow = Composite({
-        'composition': {
-            'data': 'tree[any]',
-            'model': 'sbml',
-            'parameters': 'tree[any]',
-            'simulation_results': 'tree[any]',
-            'analysis_results': 'tree[any]',
-            'parameter_estimation': {
-                '_type': 'step',
-                '_ports': {   # TODO -- this is provided by the Process schema, why is it required here?
-                    'inputs': {
-                        'data': 'tree[any]',
-                        'model': 'sbml'},
-                    'outputs': {
-                        'parameters': 'tree[any]'}}},
-            'simulator': {
-                '_type': 'step',
-                '_ports': {
-                    'inputs': {
-                        'model': 'sbml',
-                        'parameters': 'tree[any]'},
-                    'outputs': {
-                        'simulation_results': 'tree[any]'}}},
-            'analysis': {
-                '_type': 'step',
-                '_ports': {
-                    'inputs': {
-                        'simulation_results': 'tree[any]'},
-                    'outputs': {
-                        'analysis_results': 'tree[any]'}}}},
-        'schema': {
-            'results': 'tree[any]'},
-        'bridge': {
-            'results': ['analysis_results']},
-        'state': {
-            'data': {},
-            'model': '"something.sbml"',
-            'parameter_estimation': {
-                'address': 'local:process_bigraph.experiments.toys.EstimateParameters',
-                'config': {},
-                'wires': {
-                    'inputs': {
-                        'data': ['data'],
-                        'model': ['model']},
-                    'outputs': {
-                        'parameters': ['parameters']}}},
-            'simulator': {
-                'address': 'local:process_bigraph.experiments.toys.UniformTimecourse',
-                'config': {},
-                'wires': {
-                    'inputs': {
-                        'model': ['model'],
-                        'parameters': ['parameters']},
-                    'outputs': {
-                        'simulation_results': ['simulation_results']}}},
-            'analysis': {
-                'address': 'local:process_bigraph.experiments.toys.AnalyzeResults',
-                'config': {},
-                'wires': {
-                    'inputs': {
-                        'simulation_results': ['simulation_results']},
-                    'outputs': {
-                        'analysis_results': ['analysis_results']}}}}})
+def test_stochastic_deterministic_composite():
+    # TODO make the demo for a hybrid stochastic/deterministic simulator
+    pass
 
 
 if __name__ == '__main__':
