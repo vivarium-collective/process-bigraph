647117f8c6cd0f6917c7dabb947512870d02ba87
alexPatrie <alexanderpatrie@gmail.com>
2023-12-01 19:05:54 -0500

    updated db

--- process_bigraph/experiments/smoldyn_db.py
+++ process_bigraph/experiments/smoldyn_db.py
@@ -11,10 +11,14 @@ sim.addOutputData('mols')
 sim.addCommand(cmd='executiontime time', cmd_type='E')
 sim.addCommand(cmd='listmols mols', cmd_type='E')
 sim.run(1, 1)
+
 molecules = sim.getOutputData('mols')
 time = sim.getOutputData('time')
 
 emitter = DatabaseEmitter(config={'ports': {}, 'emit_limit': 9000000})
-print(molecules[0], time)
-#data = emitter.format_data(table_id='minE_0', time=)
+data = emitter.format_data(table_id='minE_0', time='1', mol_list=molecules)
+
+emitter.emit(data)
+
+
 
