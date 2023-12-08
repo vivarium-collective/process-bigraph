44db3ab6937fb2cb1be26983699422110e893a53
alexPatrie <alexanderpatrie@gmail.com>
2023-12-08 10:14:49 -0500

    updated init with pf and pp and added copasi process

--- process_bigraph/__init__.py
+++ process_bigraph/__init__.py
@@ -1,3 +1,4 @@
+import pprint
 from process_bigraph.protocols import local_lookup
 from process_bigraph.registry import protocol_registry, process_registry
 from process_bigraph.emitter import ConsoleEmitter, RAMEmitter, DatabaseEmitter
@@ -10,9 +11,25 @@ __all__ = [
     'Step',
     'Composite',
     'types',
+    'pp',
+    'pf',
     'protocol_registry',
     'process_registry'
 ]
+
+
+pretty = pprint.PrettyPrinter(indent=2)
+
+
+def pp(x):
+    """Print ``x`` in a pretty format."""
+    pretty.pprint(x)
+
+
+def pf(x):
+    """Format ``x`` for display."""
+    return pretty.pformat(x)
+
 
 # register protocols
 protocol_registry.register('local', local_lookup)
