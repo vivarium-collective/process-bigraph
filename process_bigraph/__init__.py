e001b07bcb291f0f5b09468dbc4cbf8abebcf23c
alexPatrie <alexanderpatrie@gmail.com>
2023-12-08 10:01:23 -0500

    updated init to include all main

--- process_bigraph/__init__.py
+++ process_bigraph/__init__.py
@@ -5,6 +5,15 @@ from process_bigraph.composite import Process, Step, Composite
 from process_bigraph.type_system import types
 
 
+__all__ = [
+    'Process',
+    'Step',
+    'Composite',
+    'types',
+    'protocol_registry',
+    'process_registry'
+]
+
 # register protocols
 protocol_registry.register('local', local_lookup)
 
