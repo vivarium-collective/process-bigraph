a130061a26a4f28fbdd0e7dcc4ee2ea6a3c8328c
Eran <agmon.eran@gmail.com>
2024-02-27 13:08:53 -0500

    remove all database emitter references

--- process_bigraph/__init__.py
+++ process_bigraph/__init__.py
@@ -1,16 +1,15 @@
 import pprint
 from process_bigraph.protocols import local_lookup
 from process_bigraph.registry import protocol_registry, process_registry, register_process
-from process_bigraph.emitter import ConsoleEmitter, RAMEmitter, DatabaseEmitter
+from process_bigraph.emitter import ConsoleEmitter, RAMEmitter
 from process_bigraph.composite import Process, Step, Composite
-from process_bigraph.type_system import ProcessTypes # , core
+from process_bigraph.type_system import ProcessTypes
 
 
 __all__ = [
     'Process',
     'Step',
     'Composite',
-    'core',
     'pp',
     'pf',
     'protocol_registry',
@@ -37,4 +36,3 @@ protocol_registry.register('local', local_lookup)
 # register emitters
 process_registry.register('console-emitter', ConsoleEmitter)
 process_registry.register('ram-emitter', RAMEmitter)
-process_registry.register('database-emitter', DatabaseEmitter)
