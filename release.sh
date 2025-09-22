d7391325591f394f38173592ae18dc88069066d1
Ryan Spangler <ryan.spangler@gmail.com>
2025-09-22 16:16:05 -0700

    preparing for release

--- release.sh
+++ release.sh
@@ -2,28 +2,18 @@
 # the version and publishing the version to PyPI. Also perform some
 # basic checks to avoid mistakes in releases, for example tags not
 # matching PyPI.
-# Usage: ./release.sh 0.0.1
+# Usage: ./release.sh
 
 set -e
 
-version="$1"
-
-# Check version is valid
-setup_py_version="$(python setup.py --version)"
-if [ "$setup_py_version" != "$version" ]; then
-    echo "setup.py has version $setup_py_version, not $version."
-    echo "Aborting."
-    exit 1
-fi
-
-# Check working directory is clean
+# check working directory is clean
 if [ ! -z "$(git status --untracked-files=no --porcelain)" ]; then
     echo "You have changes that have yet to be committed."
     echo "Aborting."
     exit 1
 fi
 
-# Check that we are on main
+# check that we are on main
 branch="$(git rev-parse --abbrev-ref HEAD)"
 if [ "$branch" != "main" ]; then
     echo "You are on $branch but should be on main for releases."
@@ -31,13 +21,21 @@ if [ "$branch" != "main" ]; then
     exit 1
 fi
 
-# Create and push git tag
-git tag -m "Version v$version" "v$version"
+# bump the version
+uv version --bump patch
+version=$(uv version --short)
+
+# create and push git tag
+git add pyproject.toml
+git commit -m "version $version"
+git tag -m "version v$version" "v$version"
 git push --tags
 
-# Create and publish package
-rm -rf dist
-python setup.py sdist
-twine upload dist/*
+# perform the build
+rm -rf build/ dist/
+uv build
 
-echo "Version v$version has been published on PyPI and has a git tag."
+# publish to pypi
+uv publish --token $(cat ~/.pypi-token)
+
+echo "version v$version has been published on PyPI and has a git tag."
--- setup.py
+++ setup.py
@@ -1,57 +1,10 @@
-import re
 from setuptools import setup, find_packages
-
-
-VERSION = '0.0.42'
 
 
 with open("README.md", "r") as readme:
     description = readme.read()
-    # Patch the relative links to absolute URLs that will work on PyPI.
-    description2 = re.sub(
-        r']\(([\w/.-]+\.png)\)',
-        r'](https://github.com/vivarium-collective/process-bigraph/raw/main/\1)',
-        description)
-    long_description = re.sub(
-        r']\(([\w/.-]+)\)',
-        r'](https://github.com/vivarium-collective/process-bigraph/blob/main/\1)',
-        description2)
+
 
 setup(
-    name="process-bigraph",
-    version=VERSION,
-    author="Ryan Spangler, Eran Agmon",
-    author_email="ryan.spangler@gmail.com, agmon.eran@gmail.com",
-    description="",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/vivarium-collective/process-bigraph",
-    # packages=find_packages(),
-    packages=[
-        'process_bigraph',
-        'process_bigraph.processes',
-        'process_bigraph.experiments'
-    ],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    python_requires=">=3.6",
-    install_requires=[
-        "bigraph-schema",
-        "numpy",
-        "pytest",
-        "orjson",
-        "matplotlib"
-    ]
+    name="bigraph-schema",
 )
